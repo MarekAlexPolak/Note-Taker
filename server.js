f6ed5a8be08ce4024c78b2b94cbb9f172895f4da
Marek Polak <markalexpolak@gmail.com>
2023-05-23 18:12:36 -0400

    small changes

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 7777;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname +'/public/index.html');
