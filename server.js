dd892c704ce82a7fc21921dbcedb0961bab2e640
Marek Polak <markalexpolak@gmail.com>
2023-05-23 18:02:42 -0400

    fixed variable typo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/api/notes', (req,res) => {
     res.sendFile(__dirname + './db/db.json');
 });
 
-app.post ('/api/notes', (req,res) => {
+app.post ('/api/notes', (req,req) => {
     let {title, text} = req.body;
     if (title && text) {
         let newNote = {title, text};
@@ -35,7 +35,7 @@ app.post ('/api/notes', (req,res) => {
                 console.log("this works")
             });
         })
-        res.send(newNote);
+        //res.send(newNote);
     }
     else {
         throw new Error ('it does not work')
