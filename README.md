61493d67db64e2617419de8f1be6dc09b98dcee0
Marek Polak <markalexpolak@gmail.com>
2023-05-23 22:15:40 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # Note-Taker
+
+  ## Description
+
+  A express.js application which allows a user to save and delete notes.
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+
+  - [License](#license)
+
+  - [Contact Information](#contact-information)
+
+  ## Installation
+
+  N/A
+
+  ## Usage
+
+  Go to the deployed app here: https://note-taker-marekpolak.herokuapp.com/
+
+  Click get started to enter the app.
+
+  ![Start Screen](/images/startscreen.PNG)
+
+  The left side displays previously saved notes and the right side displays the selected note.
+
+  ![Default Interface](/images/Interface.PNG)
+
+  The + allows you to add new notes.
+
+  ![Add Button](/images/addbtn.PNG)
+
+  Once a title and note are written a save button appears which allows you to save the note.
+  
+  ![Save Button](/images/SaveBtn.PNG)
+
+  ## License
+    
+    N/A
+
+  ## Contact Information
+  
+  [My Github](https://github.com/MarekAlexPolak)
+  
+  Email: markalexpolak@gmail.com
+  
--- server.js
+++ server.js
@@ -1,38 +1,47 @@
+//import required libraries
 const express = require('express');
 const fs = require('fs');
 
 //adding express functionality
 const app = express(); 
 
-//enable server
+//enable server port
 const PORT = process.env.PORT || 5009;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//get index html
 app.get('/', (req,res) => {
     res.sendFile(__dirname +'/public/index.html');
 });
-
+//get notes html
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
-
+//get existing notes files saved in the server
 app.get('/api/notes', (req,res) => {
     res.sendFile(__dirname + '/db/db.json');
 });
 
+
 app.post ('/api/notes', (req,res) => {
+    //save the give title and text of the note in their own variables
     let {title, text} = req.body;
+    //once both have been inputed
     if (title && text) {
         let newNote = {title, text};
+        //reads the existing db.json to get saved information
         fs.readFile('./db/db.json', 'utf-8', (err,data) => {
             if (err) throw err;
+            //saves original data in the new data variable
             let newData = JSON.parse(data);
             newNote.id = newData.length;
             console.log(newNote);
+            //appends the newData array with the newly inputed information
             newData.push(newNote);
+            //rewrites the db.json file to include new inputed data
             fs.writeFile('./db/db.json', JSON.stringify(newData), err =>{
                 if (err) throw err;
                 console.log("this works")
@@ -48,10 +57,12 @@ app.post ('/api/notes', (req,res) => {
 app.delete('/api/notes/:id', (req, res) => {
     // Get the id of the note being deleted
     const id = req.params.id;
+    //get information from db.json
     fs.readFile('./db/db.json', 'utf-8', (err, notes) => {
         if (err) throw err;
-    
+        //store information in an array
         delNotes = JSON.parse(notes);
+        //indexing could have been done better by using something like a hashtable
         for (let i = 0; i < delNotes.length; i++) {
             if (delNotes[i].id === parseInt(id)) {
                 delNotes.splice(i, 1);
@@ -61,6 +72,7 @@ app.delete('/api/notes/:id', (req, res) => {
                 break;
             }
         }
+        //writes the new array without the deleted entry
         fs.writeFile('./db/db.json', JSON.stringify(delNotes), err => {
             if (err) throw err;
             console.log(id);
@@ -68,7 +80,7 @@ app.delete('/api/notes/:id', (req, res) => {
         res.send(delNotes);
     });
 })
-
+//listen to the port specified
 app.listen(PORT, () => {
     console.log(`Listening on Port: ${PORT}`);
   });
